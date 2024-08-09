e3c79c26af8117cca90579a19bffc522e8c9ed4f
Isaqueopz <sergioisaquepb@gmail.com>
2024-08-09 17:59:25 -0300

    Exe finalizados

---
+++ ex19.py
@@ -0,0 +1,5 @@
+tamanhoDownload = float(input("Qual o tamanho do arquivo que você vai instalar? [em MB] "))
+velocidadeInternet = float(input("Qual é a velocidade do link de Internet utilizado ? [em MBps] "))
+tempoSegundos = tamanhoDownload / velocidadeInternet
+tempoMinutos = tempoSegundos / 60 
+print(f"O tempo aproximado de download é de {tempoMinutos:.2f} minutos.")
