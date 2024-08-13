f91896105d7fbdcbe8e83e9a12a6aacfd4b3323a
Isaque Misael <sergioisaquepb@gmail.com>
2024-08-13 17:29:57 -0300

    corrigindo code

--- ex11.py
+++ ex11.py
@@ -1,4 +1,5 @@
 valorDepositado = float(input("Digite o valor depositado: "))
 valorComJuros = valorDepositado * (0.7/100)
 valorFinal = valorComJuros + valorDepositado
-print(f"""O valor inicial era de R${valorDepositado:.2f}reais e o valor final após o seu investimento foi de R${valorFinal:.2f}reais""")
+print(f"""O valor inicial era de R${valorDepositado:.2f}reais e o valor 
+final após o seu investimento foi de R${valorFinal:.2f}reais""")
