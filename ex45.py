2bc0d37672e325d93e2c9913794354559d5766c4
Isaque Misael <sergioisaquepb@gmail.com>
2024-08-20 10:21:43 -0300

    ex45 pt2 corrigido

--- ex45.py
+++ ex45.py
@@ -1,4 +1,11 @@
-a = 5 
-b = 3
-c = a * b 
-print(c)
+altura = float(input("Altura: "))
+sexo = input("Sexo [M/F]: ")
+formula_m = (72.7 * altura) - 58
+formula_f = (62.1 * altura) - 44.7
+if sexo in "Mm":
+    print(f"Peso ideal: {formula_m:.1f}")
+else:
+    if sexo in "Ff":
+        print(f"Peso ideal: {formula_f:.1f}")
+    else:
+        print("Valor inválido")
